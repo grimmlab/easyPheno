eb9f2540e7f9121ec9e64132e40e9a303634a9a8
fhaselbeck <florian.haselbeck@hswt.de>
2022-03-28 14:40:44 +0200

    rtd tests

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,11 +23,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    "cliff.sphinxext",
-    "sphinx_copybutton",
-    "sphinx_gallery.gen_gallery",
-    "matplotlib.sphinxext.plot_directive",
-    "sphinx_plotly_directive",
     'myst_parser',
     'autoapi.extension',
 ]
