612cc816409ca21625a7c3c08af43dcfbc94e6fe
fhaselbeck <florian.haselbeck@hswt.de>
2022-04-13 14:09:46 +0200

    ignore pruned exception

--- easyPheno/optimization/optuna_optim.py
+++ easyPheno/optimization/optuna_optim.py
@@ -224,9 +224,7 @@ class OptunaOptim:
                 for metric, value in eval_metrics.get_evaluation_report(y_pred=y_pred, y_true=y_val, task=self.task,
                                                                         prefix=innerfold_name + '_').items():
                     validation_results.at[0, metric] = value
-            except optuna.exceptions.TrialPruned:
-                pass
-            except Exception as exc:
+            except (RuntimeError, TypeError, tf.errors.ResourceExhaustedError, ValueError) as exc:
                 print(exc)
                 if 'out of memory' in str(exc) or isinstance(exc, tf.errors.ResourceExhaustedError):
                     # Recover from CUDA out of memory error
