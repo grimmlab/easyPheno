aa5855e86bafbdbe5d7d45c07f9dad7e4c621a5c
fhaselbeck <florian.haselbeck@hswt.de>
2022-06-02 09:23:15 +0200

    bayes featimp + burnin + niter

--- easyPheno/model/bayes.py
+++ easyPheno/model/bayes.py
@@ -30,7 +30,6 @@ class Bayes(_param_free_base_model.ParamFreeBaseModel):
         self.mu = None
         self.beta = None
 
-
     def fit(self, X: np.array, y: np.array) -> np.array:
         """
         Implementation of fit function for Bayesian alphabet.
@@ -47,7 +46,7 @@ class Bayes(_param_free_base_model.ParamFreeBaseModel):
 
         # run BGLR for BayesB
         ETA = base.list(base.list(X=R_X, model='BayesB'))
-        fmBB = BGLR.BGLR(y=R_y, ETA=ETA, verbose=False)
+        fmBB = BGLR.BGLR(y=R_y, ETA=ETA, verbose=False, nIter=6000, burnIn=1000)
 
         # save results as numpy arrays
         self.beta = np.asarray(fmBB.rx2('ETA').rx2(1).rx2('b'))
