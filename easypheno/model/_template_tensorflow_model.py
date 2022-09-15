b2a32df3d3987c888d7febc27d9aef719244f368
fhaselbeck <florian.haselbeck@hswt.de>
2022-09-15 08:42:30 +0200

    template classes for sklearn tf torch

---
+++ easypheno/model/_template_tensorflow_model.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+
+from . import _tensorflow_model
+
+
+class TemplateTensorflowModel(_tensorflow_model.TensorflowModel):
+    """
+    Template file for a prediction model based on :obj:`~easypheno.model._tensorflow_model.TensorflowModel`
+
+    See :obj:`~easypheno.model._base_model.BaseModel` and :obj:`~easypheno.model._tensorflow_model.TensorflowModel` for more information on the attributes.
+
+    Steps you have to do to add your own model:
+        1. Copy this template file and rename it according to your model (will be the name to call it later on on the command line)
+        2. Rename the class and add it to :obj:`~easypheno.model.__init__.py`
+        3. Adjust the class attributes if necessary
+        4. Define your model in the function *define_model()*
+        5. Define the hyperparameters and ranges you want to use for optimization in *define_hyperparams_to_tune()*.
+            CAUTION: some are already defined in :obj:`~easypheno.model._tensorflow_model.TensorflowModel.common_hyperparams()`
+            and suggested in :obj:`~easypheno.model._tensorflow_model.TensorflowModel.__init__.py`. You can directly use them here.
+        6. Test your new prediction model using toy data
+    """
+    standard_encoding = ...
+    possible_encodings = [...]
+
+    def define_model(self):
+        """
+        Definition of the actual prediction model.
+
+        Use *param = self.suggest_hyperparam_to_optuna(PARAM_NAME_IN_DEFINE_HYPERPARAMS_TO_TUNE)* if you want to use
+        the value of a hyperparameter that should be optimized.
+        The function needs to return the model object.
+
+        See :obj:`~easypheno.model._base_model.BaseModel` for more information.
+        """
+        ...
+
+    def define_hyperparams_to_tune(self) -> dict:
+        """
+        Define the hyperparameters and ranges you want to optimize.
+        Caution: they will only be optimized if you add them via *self.suggest_hyperparam_to_optuna(PARAM_NAME)* in *define_model()*
+
+        See :obj:`~easypheno.model._base_model.BaseModel` for more information on the format and options.
+
+        Check :obj:`~easypheno.model._tensorflow_model.TensorflowModel` for already defined (and for some cases also suggested) hyperparameters.
+        """
+        return {
+            'example_param_1': {
+                'datatype': 'categorical',
+                'list_of_values': ['cat', 'dog', 'elephant']
+            },
+            'example_param_2': {
+                'datatype': 'float',
+                'lower_bound': 0.05,
+                'upper_bound': 0.95,
+                'step': 0.05
+            },
+            'example_param_3': {
+                'datatype': 'int',
+                'lower_bound': 1,
+                'upper_bound': 100
+            }
+        }
