c95c43b54fb05f9651e19a219377aedfe5843e42
MauraJohn <maura.john@tum.de>
2022-10-18 07:52:12 +0200

    update sim

--- easypheno/model/__init__.py
+++ easypheno/model/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     "_base_model", "_param_free_base_model", "_model_functions", "_sklearn_model", "_tensorflow_model", "_torch_model",
     "_bayesian_linreg", "_template_torch_model", "_template_sklearn_model", "_template_tensorflow_model",
-    "cnn", "linearregression", "elasticnet", "localcnn", "mlp", "randomforest", "svm", "xgboost", "blup", "bayes_ridge"]
+    "cnn", "linearregression", "elasticnet", "localcnn", "mlp", "randomforest", "svm", "xgboost", "blup", "bayes_ridge", "knn"]
 
 from . import *
