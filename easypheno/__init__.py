eda8f37c2733f6a97a2f4a62c9ff73c30cb11a64
fhaselbeck <florian.haselbeck@hswt.de>
2023-07-25 08:02:10 +0200

    new release

--- easypheno/__init__.py
+++ easypheno/__init__.py
@@ -14,6 +14,6 @@ with warnings.catch_warnings():
 
     from . import optim_pipeline
 
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 __author__ = 'Florian Haselbeck, Maura John, Dominik G. Grimm'
 __credits__ = 'GrimmLab @ TUM Campus Straubing (https://bit.cs.tum.de/)'
